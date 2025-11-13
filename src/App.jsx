5d12959e93cf47ee2020a0bd47e493f4c7fa6fa3
John Sison <132543192+jls777@users.noreply.github.com>
2025-11-13 01:06:11 -0500

    added header and nav links and menu icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,41 @@
 import "./App.css";
 
 function App() {
-  return <h1 class="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <div className="min-h-screen bg-slate-50 dark:bg-black dark:text-white">
+        <header className="bg-teal-700 text-white sticky top-0 z-10">
+          <section className="max-w-4xl mx-auto p-4 flex justify-between items-center">
+            <h1 className="text-3xl font-medium">
+              <a href="#hero">🚀 Acme Rockets</a>
+            </h1>
+            <div>
+              <button
+                id="mobile-open-button"
+                className="text-3xl sm:hidden focus:outline-none"
+              >
+                &#9776;
+              </button>
+              <nav
+                className="hidden sm:block space-x-8 text-xl"
+                aria-label="main"
+              >
+                <a href="#rockets" className="hover:opacity-90">
+                  Our Rockets
+                </a>
+                <a href="#testimonials" className="hover:opacity-90">
+                  Testimonials
+                </a>
+                <a href="#contact" className="hover:opacity-90">
+                  Contact Us
+                </a>
+              </nav>
+            </div>
+          </section>
+        </header>
+      </div>
+    </>
+  );
 }
 
 export default App;
